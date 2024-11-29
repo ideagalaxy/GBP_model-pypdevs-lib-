b0e22e35c3b174eaf875df5d4d090edf15ed898a
Kim Minsung <ideagalaxy@naver.com>
2024-11-29 14:53:42 +0900

    myhome

--- PlantSIm/GenerateCell.py
+++ PlantSIm/GenerateCell.py
@@ -8,6 +8,81 @@ from MaterialFlow import Source, Conveyor, Buffer, Drain, Station, Seperator
 
 
 
+class Two_sided_Wingbody_Cell(CoupledDEVS):
+    def __init__(self, name="Parallel_Cell", line_num = 2, task_num = 3, cycle_time = 6):
+        CoupledDEVS.__init__(self, name)
+
+        if line_num < 2:
+            raise ValueError("2개 이상부터 Cell 제작 가능")
+
+        self.seperator = self.addSubModel(Seperator(name="seperator",out_way=line_num))
+        self.buffer = self.addSubModel(Buffer(name="buffer"))
+
+        self.inport = self.addInPort(name="Two_sided_Wingbody_Cell_in")
+        self.outport = self.addOutPort(name="Two_sided_Wingbody_Cell_out")
+        self.response_inport = self.addInPort(name="Two_sided_Wingbody_Cell_response_inport")
+
+        self.connectPorts(self.inport, self.seperator.inport)
+        self.connectPorts(self.response_inport, self.buffer.response_inport)
+        self.connectPorts(self.buffer.outport, self.outport)
+    
+        tmp1 = ["seperator"]
+        tmp2 = []
+        tmp3 = []
+        
+        conv_lengths = parrallel_generate_odd(line_num)
+        i=0
+        for line in range(line_num):
+            if is_odd(line_num):
+                if line == 0:
+                    continue
+            print("line : ",line)
+            
+            conveyor_in_name = "line"+str(line)+"_conveyor_in"
+            setattr(self,conveyor_in_name,self.addSubModel(Conveyor(name=conveyor_in_name, length=conv_lengths[i])))
+            tmp1.append(conveyor_in_name)
+            #print(f"{self.name} : {conveyor_in_name} : setting {conv_lengths[i]} m")
+            
+            task_name = "line"+str(line)+"_task"
+            setattr(self,task_name,self.addSubModel(Station(name=task_name, working_time=cycle_time)))
+            tmp2.append(task_name)
+            #print(f"{self.name} : line{line}_station X {task_num} : setting {cycle_time} sec")
+
+            conveyor_out_name = "line"+str(line)+"_conveyor_out"
+            setattr(self,conveyor_out_name,self.addSubModel(Conveyor(name=conveyor_out_name, length=conv_lengths[i])))
+            tmp3.append(conveyor_out_name)
+            #print(f"{self.name} : {conveyor_out_name}: setting {conv_lengths[i]} m")
+
+            print(f"{self.name} : conveyor_in : {conv_lengths[i]}m | task X {task_num} | conveyor_out : {conv_lengths[i]}m")
+
+            conv_in = getattr(self, conveyor_in_name)
+            task = getattr(self, task_name)   
+            conv_out = getattr(self, conveyor_out_name)
+
+            outport_num = "outport_"+str(line)
+            seperator_outport = getattr(self.seperator, outport_num)
+            self.connectPorts(seperator_outport, conv_in.inport)
+
+            response_port_num = "response_inport_"+str(line)
+            seperator_response_inport = getattr(self.seperator, response_port_num)
+            self.connectPorts(conv_in.outport, seperator_response_inport)
+
+            self.connectPorts(conv_in.outport, task.inport)
+            self.connectPorts(task.response_outport, conv_in.response_inport)
+            self.connectPorts(task.outport, conv_out.inport)
+            self.connectPorts(conv_out.outport, self.buffer.inport)
+
+            i += 1
+        tmp3.append("buffer")
+
+        self.variable = tmp1 + tmp2 + tmp3
+
+    def select(self, imm):
+        for var_name in reversed(self.variable):
+            var_value = getattr(self, var_name)
+            if var_value in imm:
+                return var_value 
+
 
 class Tasks_Cell(CoupledDEVS):
     def __init__(self, name="Tasks_Cell", task_num = 3, param = 12):
